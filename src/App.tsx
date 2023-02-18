e1864d68c1c446b22c3e5810ed871218a54c4543
claytonfortunato <claytonreis10@gmail.com>
2023-02-18 17:46:26 -0300

    feat: Api

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
 import * as C from "./styles";
-import React from "react";
+import { ChangeEvent, useState } from "react";
 
-const App = () => {
+const App = (): JSX.Element => {
+  const [term, setTerm] = useState<string>("");
+
+  const getSearchOptions = (value: string) => {
+    fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${value.trim()}&limit=5&lang=en&appid=${
+        process.env.REACT_APP_API_KEY
+      }`
+    )
+      .then((res) => res.json())
+      .then((data) => console.log({ data }));
+  };
+
+  const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value.trim();
+
+    if (value === "") return;
+
+    setTerm(e.target.value);
+    getSearchOptions(value);
+  };
+
   return (
     <C.Container>
       <C.Header>Previsão do tempo</C.Header>
       <p>Digite abaixo um lugar do qual você deseja saber o tempo</p>
       <C.Search>
-        <input type="text" value={""} />
+        <input type="text" value={term} onChange={onInputChange} />
         <button>Pesquisar</button>
       </C.Search>
     </C.Container>
